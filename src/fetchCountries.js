716be714e970aa025e0fef5292b61ccb534a88be
SirotovAlexander <alexander.sirotov@gmail.com>
2023-04-28 09:53:26 +0300

    done-first-part
    done-first-part

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,19 +1,14 @@
-const END_POINT = 'https://restcountries.com/v3.1/all?fields=';
-const KEY_VALUES = 'name,capital,population,flags,languages';
+const PARAMS = 'name,capital,population,flags,languages';
 
-function fetchCountries() {
-  const URL = `${END_POINT}${KEY_VALUES}`;
+function fetchCountries(name) {
+  const URL = `https://restcountries.com/v3.1/name/${name}?${PARAMS}`;
+
   return fetch(URL).then(resp => {
-    console.log(URL);
     if (!resp.ok) {
       throw new Error(resp.statusText);
     }
     return resp.json();
   });
 }
-
-fetchCountries()
-  .then(data => console.log(data))
-  .catch(error => console.log(error));
 
 export { fetchCountries };
--- src/index.js
+++ src/index.js
@@ -4,4 +4,35 @@ const DEBOUNCE_DELAY = 300;
 import debounce from 'lodash.debounce';
 import Notiflix from 'notiflix';
 import { fetchCountries } from './fetchCountries';
-// import './css/styles.css';
+const input = document.querySelector('#search-box');
+const list = document.querySelector('.country-list');
+const info = document.querySelector('.country-info');
+
+input.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY));
+
+function onSearch(event) {
+  const nameOfCountry = event.target.value.trim();
+  if (!nameOfCountry) {
+    list.innerHTML = '';
+    info.innerHTML = '';
+  }
+  fetchCountries(nameOfCountry)
+    .then(data => choisOneOfArray(data))
+    .catch(error => console.log(error));
+}
+
+function choisOneOfArray(listOfcountries) {
+  if (listOfcountries.length > 10) {
+    Notiflix.Notify.info(
+      'Too many matches found. Please enter a more specific name.'
+    );
+  } else if (listOfcountries.length <= 5) {
+    console.log(listOfcountries);
+  } else if (listOfcountries.length === 1) {
+    console.log(listOfcountries);
+  }
+}
+
+console.log(input);
+console.log(list);
+console.log(info);
